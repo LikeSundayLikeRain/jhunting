4b7b273131ce4a4093149ad6ae95e69de3f38a72
Unknown <monsoon1013@gmail.com>
2018-08-29 23:06:08 -0400

    Initial commit

---
+++ src/container/register/register.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../../component/logo/logo'
+
+class Register extends React.Component {
+    render() {
+        return (<div>
+            <Logo></Logo>
+            <h2>Login</h2>
+        </div>)
+    }
+}
+
+export default Register
